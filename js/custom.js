18e1a6afcba606f6f0d9e533837d0dce0bfd53c2
Chetan Kalra <chetankalra11@gmail.com>
2018-04-11 22:45:31 +0530

    Front End

--- js/custom.js
+++ js/custom.js
@@ -1,3 +1,42 @@
+$(document).ready(function() {
+    $("#lightSlider").lightSlider({
+        item: 2,
+        autoWidth: false,
+        slideMove: 1,
+        slideMargin: 15,
+        speed: 400, //ms'
+        auto: true,
+        loop: true,
+        freeMove:true,
+        pause:5000,
+        responsive : [
+          {
+              breakpoint:768,
+              settings: {
+                  item:1,
+                  slideMove:1,
+                  slideMargin:6,
+                }
+          }
+      ]
+    });
+});
+
+$(document).ready(function() {
+    $("#lightSlider-comments").lightSlider({
+        item: 1,
+        autoWidth: false,
+        slideMove: 1,
+        slideMargin: 15,
+        speed: 400, //ms'
+        auto: true,
+        loop: true,
+        freeMove:true,
+        pause:5000,
+        controls: false,
+    });
+});
+
 $('.mobile-menu-trigger').click(function(){
 
     var check = $( "#nav-links" ).hasClass( "collapse-in")
@@ -52,4 +91,18 @@ jQuery(window).on('scroll', function(){
 jQuery('#toTop').on('click', function(){
     jQuery("html, body").animate({ scrollTop: 0 }, 700);
     return false; 
+});
+
+$("#to-comments-section").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#comments-section").offset().top
+    }, 1300);
+});
+
+$(".user-reply").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#user-comment-row").offset().top
+    }, 1300);
+
+    $('#comment').focus();
 });
